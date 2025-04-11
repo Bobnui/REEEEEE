549738009687435a19ce765f2dfc9f3de45452ae
Bobnui <matarikihpi@gmail.com>
2025-04-11 12:03:20 +1200

    Menu quit and game quit

--- Assets/MyContent/MyScripts/SceneHandler.cs
+++ Assets/MyContent/MyScripts/SceneHandler.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,5 +7,14 @@ public class SceneHandler : MonoBehaviour
     public void OpenMainMenu()
     {
         SceneManager.LoadScene(1);
+    }
+    public void QuitTheGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+
+#endif
     }
 }
